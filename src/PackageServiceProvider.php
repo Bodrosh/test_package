a617c50ac5a1763606543a48a0957a87a85333a9
Andrey Noikov <a.novikov@whitewill.ru>
2024-09-20 08:33:31 +0300

    first com

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Bodrosh\TestPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Регистрация всех сервисов пакета.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Регистрация зависимостей пакета
+    }
+
+    /**
+     * Загрузка всех ресурсов пакета.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dump(11);
+        // Загрузка маршрутов, если есть
+//        if (file_exists(__DIR__.'/../routes/web.php')) {
+//            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+//        }
+//
+//        // Загрузка конфигураций, если есть
+//        if (file_exists(__DIR__.'/../config/package.php')) {
+//            $this->mergeConfigFrom(__DIR__.'/../config/package.php', 'package');
+//            $this->publishes([
+//                __DIR__.'/../config/package.php' => config_path('package.php'),
+//            ], 'config');
+//        }
+//
+//        // Загрузка представлений, если есть
+//        if (is_dir(__DIR__.'/../resources/views')) {
+//            $this->loadViewsFrom(__DIR__.'/../resources/views', 'package');
+//            $this->publishes([
+//                __DIR__.'/../resources/views' => resource_path('views/vendor/package'),
+//            ], 'views');
+//        }
+//
+//        // Загрузка переводов, если есть
+//        if (is_dir(__DIR__.'/../resources/lang')) {
+//            $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'package');
+//        }
+    }
+}
